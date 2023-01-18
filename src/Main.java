166eb5d0f1221a34b15022bfbd0bf591d49b136b
Kimtai <kymdennis3@gmail.com>
2023-01-18 04:58:21 +0300

    another way to write for-each loop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /*for - each = traversing technique to iterate through the elements in an array/collections
+        *              less steps,more readable
+        *              less flexible*/
+
+        //String [] animals = {"cats", "dogs", "birds", "lion"};
+        ArrayList<String> animals = new ArrayList<>();
+
+        animals.add("cats");
+        animals.add("dogs");
+        animals.add("lions");
+        animals.add("birds");
+
+
+        for(String i: animals){
+            System.out.println(i);
+        }
     }
 }
